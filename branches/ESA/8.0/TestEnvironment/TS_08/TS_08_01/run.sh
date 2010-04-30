c1f21d8ab2bfc04ffcd2bafa8e2c81e7e6653827
fanciulli <fanciulli@df02127e-7e62-ee8a-d59c-71b5b3ac54bb>
2010-04-30 13:22:09 +0000

    Updated test case

--- branches/ESA/8.0/TestEnvironment/TS_08/TS_08_01/run.sh
+++ branches/ESA/8.0/TestEnvironment/TS_08/TS_08_01/run.sh
@@ -1,1 +1,1 @@
-java -cp ../tcpTest.jar  it.intecs.pisa.soap.toolbox.test.TCPTest 13269 /home/toolbox/TestEnvironment/Responses/RFQ_response_TS_08_01.xml  /home/toolbox/TestEnvironment/Responses/Order_response_TS_08_01.xml     
+java -cp ../tcpTest.jar  it.intecs.pisa.soap.toolbox.test.TCPTest 13269 ../../Responses/RFQ_response_TS_08_01.xml  ../../Responses/Order_response_TS_08_01.xml     

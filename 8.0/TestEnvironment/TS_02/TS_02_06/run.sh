bb0b97064cadfb56fe37bff36a19fa8898e8cfbb
fanciulli@gmail.com <fanciulli@gmail.com@df02127e-7e62-ee8a-d59c-71b5b3ac54bb>
2010-05-13 09:03:13 +0000

    removed from Test Plan

--- 8.0/TestEnvironment/TS_02/TS_02_06/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm ../../InputDir/TS_02_02.xml
-rm ../..//OutputDir/TS_02_02.xml
-
-java -jar ../../Code/TestTool.jar CopyFileWhenResourceAvailable ../../InputDir/TS_02_02.xml ../..//OutputDir/TS_02_02.xml ../../Responses/Order_response_TS_02_02.xml

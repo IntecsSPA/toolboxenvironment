8098e3d5b40336266e38dcaaa78019ca90498557
fanciulli <fanciulli@df02127e-7e62-ee8a-d59c-71b5b3ac54bb>
2010-05-24 14:03:18 +0000

    Fixed isSOAPMessage method

--- 8.0/Utils/src/it/intecs/pisa/util/SOAPUtil.java
+++ 8.0/Utils/src/it/intecs/pisa/util/SOAPUtil.java
@@ -15,6 +15,9 @@ import org.w3c.dom.Element;
 public class SOAPUtil {
     public static final String FAULT_NAMESPACE = "http://schemas.xmlsoap.org/soap/envelope/";
     public static final String FAULT_ROOT_NODE = "Fault";
+    public static final String ENVELOPE_ROOT_NODE = "Envelope";
+    public static final String BODY_NODE = "Body";
+    
     public static boolean isSOAPFault(Document doc)
     {
         Element rootEl;
@@ -28,12 +31,34 @@ public class SOAPUtil {
             name=rootEl.getNodeName();
 
             if(namespace.equals(FAULT_NAMESPACE) && name.endsWith(FAULT_ROOT_NODE))
-                return true;
+                return isBosySOAPFault(rootEl);
+            else if(namespace.equals(FAULT_NAMESPACE) && name.endsWith(ENVELOPE_ROOT_NODE))
+            {
+            	Element bodyEl;
+            	Element faultEl;
+            	
+            	bodyEl=DOMUtil.getChildByLocalName(rootEl,BODY_NODE);
+            	faultEl=DOMUtil.getFirstChild(bodyEl);
+            	return isBosySOAPFault(faultEl);
+            }
             else return false;
         }
         catch(Exception e)
         {
             return false;
         }
+    }
+    
+    protected static boolean isBosySOAPFault(Element el)
+    {
+    	String namespace;
+        String name;
+        
+    	namespace=el.getNamespaceURI();
+        name=el.getNodeName();
+
+        if(namespace.equals(FAULT_NAMESPACE) && name.endsWith(FAULT_ROOT_NODE))
+            return true;
+        else return false;
     }
 }

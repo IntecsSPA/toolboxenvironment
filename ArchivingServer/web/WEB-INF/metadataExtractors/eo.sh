82ba21ff3affef2930fa01de5dcee2f11d2b0a6d
fanciulli <fanciulli@df02127e-7e62-ee8a-d59c-71b5b3ac54bb>
2010-06-25 15:20:56 +0000

    Adding documentation

---
+++ ArchivingServer/web/WEB-INF/metadataExtractors/eo.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is an example for future integration
+# each script will be put in execution with two parameters:
+# 1) full path of the data. The script shall assume to have data on that path
+# 2) full path of the metadata. The script shall assume to create metadata and leave it at the path.
+
+wget http://pisa007.pisa.intecs.it/testFiles/ARMS/metadataForExtractionExample.xml
+mv metadataForExtractionExample.xml $2

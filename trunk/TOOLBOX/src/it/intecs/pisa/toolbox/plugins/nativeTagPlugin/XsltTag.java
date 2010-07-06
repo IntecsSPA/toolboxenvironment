6cf6d18e71826207c24862386af06acb51697c03
gisimone <gisimone@df02127e-7e62-ee8a-d59c-71b5b3ac54bb>
2010-07-06 13:14:56 +0000

    fixed the saxon invocation issue on the XSLT tag

--- trunk/TOOLBOX/src/it/intecs/pisa/toolbox/plugins/nativeTagPlugin/XsltTag.java
+++ trunk/TOOLBOX/src/it/intecs/pisa/toolbox/plugins/nativeTagPlugin/XsltTag.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.StringWriter;
 import java.util.Iterator;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
+//import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -28,13 +28,14 @@ public class XsltTag extends NativeTagExecutor {
 
         xsltRef=executeChildTag((Element) children.next());
 
+
         if(xsltRef instanceof String)
-            transformer= TransformerFactory.newInstance().newTemplates(
+            transformer= net.sf.saxon.TransformerFactoryImpl.newInstance().newTemplates(
                 new StreamSource(new File((String) xsltRef))).newTransformer();
         else if(xsltRef instanceof Document)
-            transformer= TransformerFactory.newInstance().newTemplates(
+            transformer= net.sf.saxon.TransformerFactoryImpl.newInstance().newTemplates(
                 new DOMSource((Document) xsltRef)).newTransformer();
-        else throw new Exception("XSLT reference typ enot supported");
+        else throw new Exception("XSLT reference type not supported");
         Element child;
         while (children.hasNext()) {
             transformer.setParameter((child = (Element) children.next()).getAttribute(NAME),
@@ -56,6 +57,50 @@ public class XsltTag extends NativeTagExecutor {
             return stringResponse;
         }
     }
-    
+
+/*
+ 
+    public Object executeTag(org.w3c.dom.Element xslt) throws Exception {
+        DOMUtil domUtil = new DOMUtil();
+        String stringResponse;
+        Object xsltRef;
+        Transformer transformer;
+
+        Iterator children = DOMUtil.getChildren(xslt).iterator();
+        DOMSource domSource = new DOMSource((Document) this.executeChildTag((Element) children.next()));
+
+        xsltRef=executeChildTag((Element) children.next());
+
+        if(xsltRef instanceof String)
+            transformer= TransformerFactory.newInstance().newTemplates(
+                new StreamSource(new File((String) xsltRef))).newTransformer();
+        else if(xsltRef instanceof Document)
+            transformer= TransformerFactory.newInstance().newTemplates(
+                new DOMSource((Document) xsltRef)).newTransformer();
+        else throw new Exception("XSLT reference type not supported");
+        Element child;
+        while (children.hasNext()) {
+            transformer.setParameter((child = (Element) children.next()).getAttribute(NAME),
+                    executeChildTag(
+                    DOMUtil.getFirstChild(child)));
+        }
+        if (getBool(xslt.getAttribute(XML_OUTPUT))) {
+            Document document = domUtil.newDocument();
+            transformer.transform(domSource, new DOMResult(document));
+            
+            dumpResourceAndAddToDebugTree(document);
+            return document;
+        } else {
+            StringWriter out = new StringWriter();
+            transformer.transform(domSource, new StreamResult(out));
+            
+            stringResponse=out.toString();
+            dumpResourceAndAddToDebugTree(stringResponse);
+            return stringResponse;
+        }
+    }
+ */
+
+
  
 }

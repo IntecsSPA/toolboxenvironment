05e4a6d55af6d63c312eb471d9529f6169a4b3aa
fanciulli <fanciulli@df02127e-7e62-ee8a-d59c-71b5b3ac54bb>
2010-09-24 13:44:25 +0000

    Aligning with 8.0 version

--- trunk/Plugins/DE/com.intecs.ToolboxScript/src/it/intecs/pisa/develenv/model/interfaces/ContentChangeNotifier.java
+++
@@ -1,7 +0,0 @@
-package it.intecs.pisa.develenv.model.interfaces;
-
-import org.eclipse.jface.text.Document;
-
-public interface ContentChangeNotifier {
-	public void contentChanged(String newContent);
-}

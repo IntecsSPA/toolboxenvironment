beee5404a813703b3cfa552c50a9ad72742c9bfb
gisimone <gisimone@df02127e-7e62-ee8a-d59c-71b5b3ac54bb>
2010-12-01 11:21:02 +0000

    Updated the timer management.
    Added the Schema navigation.

--- TOOLBOX/src/it/intecs/pisa/toolbox/plugins/restManagerNativePlugins/DeleteTimerByInstanceId.java
+++ TOOLBOX/src/it/intecs/pisa/toolbox/plugins/restManagerNativePlugins/DeleteTimerByInstanceId.java
@@ -2,9 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package it.intecs.pisa.toolbox.plugins.restManagerNativePlugins;
-
 
 import com.google.gson.JsonObject;
 import it.intecs.pisa.pluginscore.RESTManagerCommandPlugin;
@@ -12,6 +10,7 @@ import it.intecs.pisa.toolbox.db.ToolboxInternalDatabase;
 import it.intecs.pisa.toolbox.timers.TimerManager;
 import it.intecs.pisa.util.json.JsonErrorObject;
 import it.intecs.pisa.util.json.JsonSuccessObject;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.StringTokenizer;
 
@@ -19,22 +18,38 @@ import java.util.StringTokenizer;
  *
  * @author simone
  */
-public class DeleteTimerByInstanceId extends RESTManagerCommandPlugin{
-     @Override
-    public JsonObject executeCommand(String method, JsonObject request) throws Exception  {
+public class DeleteTimerByInstanceId extends RESTManagerCommandPlugin {
+
+    @Override
+    public JsonObject executeCommand(String method, JsonObject request) throws Exception {
         StringTokenizer tokenizer;
         ToolboxInternalDatabase db;
         Statement stm = null;
         String sql;
+        ResultSet res = null;
         try {
             tokenizer = new StringTokenizer(method, "/");
             tokenizer.nextToken();
             tokenizer.nextToken();
             tokenizer.nextToken();
-            String instanceID=tokenizer.nextToken();
-            TimerManager.getInstance().deleteTask(instanceID);
+            String instanceID = tokenizer.nextToken();
 
-            sql = "DELETE FROM T_TIMERS WHERE TYPE='TIMER' and INSTANCE_ID="+ instanceID;
+
+            sql = "select EXTRA_VALUE from T_TIMERS where TYPE='TIMER' and INSTANCE_ID=" + instanceID;
+            db = ToolboxInternalDatabase.getInstance();
+            stm = db.getStatement();
+            res = stm.executeQuery(sql);
+            String timerID;
+            JsonObject timers = null;
+
+            int resSize = 0;
+
+            while (res.next()) {
+                timerID = res.getString("extra_value");
+                TimerManager.getInstance().deleteTask(timerID);
+
+            }
+            sql = "DELETE FROM T_TIMERS WHERE TYPE='TIMER' and INSTANCE_ID=" + instanceID;
 
             db = ToolboxInternalDatabase.getInstance();
 
@@ -42,10 +57,8 @@ public class DeleteTimerByInstanceId extends RESTManagerCommandPlugin{
             stm.executeUpdate(sql);
 
             return JsonSuccessObject.get();
-        }
-        catch(Exception e)
-        {
-            return JsonErrorObject.get("Unable to delete service");
+        } catch (Exception e) {
+            return JsonErrorObject.get("Unable to delete timer task");
         }
     }
 }

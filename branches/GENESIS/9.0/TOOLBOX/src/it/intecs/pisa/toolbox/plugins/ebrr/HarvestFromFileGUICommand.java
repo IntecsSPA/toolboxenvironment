d6e3190034f5c93d323102338abf9a9482a7e40a
andreamarongiu@gmail.com <andreamarongiu@gmail.com@df02127e-7e62-ee8a-d59c-71b5b3ac54bb>
2011-01-26 17:56:36 +0000

    Harvest Update

--- branches/GENESIS/9.0/TOOLBOX/src/it/intecs/pisa/toolbox/plugins/ebrr/HarvestFromFileGUICommand.java
+++ branches/GENESIS/9.0/TOOLBOX/src/it/intecs/pisa/toolbox/plugins/ebrr/HarvestFromFileGUICommand.java
@@ -5,7 +5,6 @@
 package it.intecs.pisa.toolbox.plugins.ebrr;
 
 import it.intecs.pisa.toolbox.Toolbox;
-import it.intecs.pisa.toolbox.configuration.ToolboxConfiguration;
 import it.intecs.pisa.toolbox.configuration.ToolboxNetwork;
 import it.intecs.pisa.toolbox.constants.MiscConstants;
 import it.intecs.pisa.toolbox.constants.ToolboxFoldersFileConstants;
@@ -16,7 +15,16 @@ import it.intecs.pisa.toolbox.plugins.managerNativePlugins.*;
 import it.intecs.pisa.util.DOMUtil;
 import it.intecs.pisa.util.IOUtil;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.fileupload.FileItem;
@@ -25,11 +33,25 @@ import org.w3c.dom.Element;
 
 /**
  *
- * @author Massimiliano
+ * @author Massimiliano Fanciulli
+ * @author Andrea Manrongiu
  */
 public class HarvestFromFileGUICommand extends NativeCommandsManagerPlugin {
 
-    public void executeCommand(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    private static final String RESOURCE_TYPE_NAME = "EarthObservation";
+    private static final String HARVEST_SOAP_ACTION = "http://www.opengis.net/cat/csw/2.0.2/requests#Harvest";
+    private static final String CSW_NAMESPACE = "http://www.opengis.net/cat/csw/2.0.2";
+    private static final String XML_SCHEMA_NAMESPACE = "http://www.w3.org/2001/XMLSchema";
+    private static final String XML_SCHEMA_INSTANCE_NAMESPACE = "http://www.w3.org/2001/XMLSchema-instance";
+    private static final String CSW_VERSION = "2.0.2";
+    private static final String CSW_SERVICE_NAME = "CSW";
+
+    private static final String HARVEST_ELEMENT_NAME = "Harvest";
+    private static final String SOURCE_ELEMENT_NAME = "Source";
+    private static final String RESOURCE_TYPE_ELEMENT_NAME = "ResourceType";
+    private static final String SERVICE_EXCEPTION_ELEMENT_NAME = "ServiceExceptionReport";
+
+    public void executeCommand(HttpServletRequest req, HttpServletResponse resp) throws Exception{
         Hashtable<String, FileItem> mimeparts;
         FileItem harvestFile;
         mimeparts = parseMultiMime(req);
@@ -37,72 +59,114 @@ public class HarvestFromFileGUICommand extends NativeCommandsManagerPlugin {
         DOMUtil domutil = new DOMUtil();
         Toolbox toolboxServlet = Toolbox.getInstance();
         logger = toolboxServlet.getLogger();
+        Document harvestDoc=null;
+        String servicename = getStringFromMimeParts(mimeparts, "serviceName");
+        ZipFile zipFile=null;
+        boolean harvestResult=false;
+        boolean list=true;
+        int countMetadata=0;
 
-// Dump message on a public directory
-        Document harvestDoc = domutil.inputStreamToDocument(harvestFile.getInputStream());
-        String filename = java.util.UUID.randomUUID().toString() + ".xml";
-        File harvestDir = new File(toolboxServlet.getRootDir(), ToolboxFoldersFileConstants.HARVEST);
-        File harvest = new File(harvestDir, filename);
-        DOMUtil.dumpXML(harvestDoc, harvest);
+        File harvestData= File.createTempFile(java.util.UUID.randomUUID().toString(), "dat");
+        IOUtil.copy(harvestFile.getInputStream(), new FileOutputStream(harvestData));
+        try {
+             zipFile = new ZipFile(harvestData);
+        } catch (ZipException exZip) {
+            /*Single Metadata*/
+                list=false;
+                harvestDoc = domutil.inputStreamToDocument(new FileInputStream(harvestData));
+                try {
+                    harvestResult=this.harvestDocument(servicename, harvestDoc);
+
+                } catch (Exception ex) {
+                    harvestResult=false;
+                }
+                 if(harvestResult)
+                   countMetadata++;
+        }
+        
+        /*Metadata List*/
+        
+        if(list){
+            ZipEntry entry= null;
+            Enumeration entries=zipFile.entries();
+            while(entries.hasMoreElements()) {
+                  entry = (ZipEntry)entries.nextElement();
+                  harvestDoc = domutil.inputStreamToDocument(zipFile.getInputStream(entry));
+                  try {
+                        harvestResult=this.harvestDocument(servicename, harvestDoc);
+                  } catch (Exception ex) {
+                    harvestResult=false;
+                  }
+                  if(harvestResult)
+                     countMetadata++;
+            }
+            zipFile.close();
+        }
+            
+        if(countMetadata==0)
+            resp.sendRedirect("ebrrHarvestFromDisk.jsp?serviceName="+servicename+"&error="+resp.encodeRedirectURL("An error occurred while harvesting data from disk"));
+        else
+            resp.sendRedirect("ebrrHarvestFromDisk.jsp?serviceName="+servicename+"&info="+resp.encodeRedirectURL("Harvest Completed"));
+    }
 
 
-// Calculate the URL
-        String source = ToolboxNetwork.getEndpointURL()+ MiscConstants.SLASH+ToolboxFoldersFileConstants.HARVEST+MiscConstants.SLASH+filename;
-
-        String servicename = "";
+    private boolean harvestDocument(String serviceName, Document harvestDocument) throws Exception{
+        DOMUtil util=null;
         TBXService service;
         Document message;
         Document soapMessage;
-        DOMUtil util;
         Element rootEl;
         Element sourceEl;
         Element resourceTypeEl;
         ServiceManager serviceManager;
+        Toolbox toolboxServlet = Toolbox.getInstance();
 
-        try {
-            util = new DOMUtil();
-            servicename = getStringFromMimeParts(mimeparts, "serviceName");
-            serviceManager = ServiceManager.getInstance();
-            service = serviceManager.getService(servicename);
-            message = util.newDocument();
+        String filename = java.util.UUID.randomUUID().toString() + ".xml";
+        String metadateNamespace=harvestDocument.getDocumentElement().getNamespaceURI();
+        String resourceTypePrefix=metadateNamespace.substring(metadateNamespace.lastIndexOf("/")+1);
+        File harvestDir = new File(toolboxServlet.getRootDir(), ToolboxFoldersFileConstants.HARVEST);
+        File harvest = new File(harvestDir, filename);
+        DOMUtil.dumpXML(harvestDocument, harvest);
+        util = new DOMUtil();
+        serviceManager = ServiceManager.getInstance();
+        service = serviceManager.getService(serviceName);
+        message = util.newDocument();
 
-            rootEl = message.createElement("Harvest");
-            rootEl.setAttribute("service", "CSW");
-            rootEl.setAttribute("version", "2.0.2");
-            rootEl.setAttribute("xmlns:csw", "http://www.opengis.net/cat/csw/2.0.2");
-            rootEl.setAttribute("xmlns", "http://www.opengis.net/cat/csw/2.0.2");
-            rootEl.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
-            rootEl.setAttribute("xmlns:xsd", "http://www.w3.org/2001/XMLSchema");
+        String source = ToolboxNetwork.getEndpointURL()+ MiscConstants.SLASH+ToolboxFoldersFileConstants.HARVEST+MiscConstants.SLASH+filename;
 
-            message.appendChild(rootEl);
+        rootEl = message.createElement(HARVEST_ELEMENT_NAME);
+        rootEl.setAttribute("service", CSW_SERVICE_NAME);
+        rootEl.setAttribute("version", CSW_VERSION);
+        rootEl.setAttribute("xmlns:csw", CSW_NAMESPACE);
+        rootEl.setAttribute("xmlns", CSW_NAMESPACE);
+        rootEl.setAttribute("xmlns:xsi", XML_SCHEMA_INSTANCE_NAMESPACE);
+        rootEl.setAttribute("xmlns:xsd", XML_SCHEMA_NAMESPACE);
+        message.appendChild(rootEl);
 
-            sourceEl = message.createElement("Source");
-            sourceEl.setTextContent(source);
-            rootEl.appendChild(sourceEl);
+        sourceEl = message.createElement(SOURCE_ELEMENT_NAME);
+        sourceEl.setAttribute("xmlns", CSW_NAMESPACE);
+        sourceEl.setTextContent(source);
+        rootEl.appendChild(sourceEl);
 
-            resourceTypeEl = message.createElement("ResourceType");
-            resourceTypeEl.setTextContent(req.getParameter("resourceType"));
-            rootEl.appendChild(resourceTypeEl);
+        resourceTypeEl = message.createElement(RESOURCE_TYPE_ELEMENT_NAME);
+        resourceTypeEl.setTextContent(resourceTypePrefix+":"+RESOURCE_TYPE_NAME);
+        resourceTypeEl.setAttribute("xmlns", CSW_NAMESPACE);
+        rootEl.appendChild(resourceTypeEl);
 
-            soapMessage = Util.addSOAPElements(message);
+        soapMessage = Util.addSOAPElements(message);
 
-            File tmpFile;
-            tmpFile = IOUtil.getTemporaryFile();
-            DOMUtil.dumpXML(soapMessage, tmpFile);
+        File tmpFile;
+        tmpFile = IOUtil.getTemporaryFile();
+        DOMUtil.dumpXML(soapMessage, tmpFile);
+        Document harvestResponse=service.processRequest(HARVEST_SOAP_ACTION, util.fileToDocument(tmpFile), false);
 
-            service.processRequest("http://www.opengis.net/cat/csw/2.0.2/requests#Harvest", util.fileToDocument(tmpFile), false);
+         // clean the HARVEST directory
+         if(harvest.exists())
+            harvest.delete();
 
-            // clean the HARVEST directory
-            if(harvest.exists())
-                harvest.delete();
-
-            resp.sendRedirect("ebrrHarvestFromDisk_showresult.jsp");
-        } catch (Exception ex) {
-            
-            if(harvest.exists())
-                harvest.delete();
-
-            resp.sendRedirect("ebrrHarvestFromDisk_showresult.jsp?error="+resp.encodeRedirectURL("An error occurred while harvesting data from disk"));
-        }
+        if(harvestResponse.getDocumentElement().getLocalName().equalsIgnoreCase(SERVICE_EXCEPTION_ELEMENT_NAME))
+           return false;
+        else
+           return true;
     }
 }

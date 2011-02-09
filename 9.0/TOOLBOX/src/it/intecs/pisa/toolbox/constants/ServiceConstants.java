44b95b8bb95d09afbec388c09830964f3b961af3
andreamarongiu@gmail.com <andreamarongiu@gmail.com@df02127e-7e62-ee8a-d59c-71b5b3ac54bb>
2011-02-09 17:08:34 +0000

    Service Security Removed for import/export services

--- 9.0/TOOLBOX/src/it/intecs/pisa/toolbox/constants/ServiceConstants.java
+++ 9.0/TOOLBOX/src/it/intecs/pisa/toolbox/constants/ServiceConstants.java
@@ -10,4 +10,6 @@ public class ServiceConstants {
 
     public static String SERVICE_DESCRIPTOR_FILE_NAME="serviceDescriptor.xml";
 
+    public static String SERVICE_SERVICE_RESOURCE_FOLDER="Resources/Security";
+
 }

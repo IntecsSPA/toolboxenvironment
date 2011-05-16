ea0a0d0a7d520d2f0df6cdea6ae7539ff1a7c4b3
andreamarongiu <andreamarongiu@df02127e-7e62-ee8a-d59c-71b5b3ac54bb>
2011-05-16 09:50:47 +0000

    Chain Types and Watches Updated

--- 9.0/ArchivingServer/src/java/it/intecs/pisa/archivingserver/chain/commands/PublishToEbRIMCatalogue.java
+++ 9.0/ArchivingServer/src/java/it/intecs/pisa/archivingserver/chain/commands/PublishToEbRIMCatalogue.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package it.intecs.pisa.archivingserver.chain.commands;
 
 import com.sun.org.apache.xpath.internal.XPathAPI;

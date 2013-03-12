580fd878f702d0822a96b61b2946bee502194079
mariarosaria.7@gmail.com <mariarosaria.7@gmail.com@df02127e-7e62-ee8a-d59c-71b5b3ac54bb>
2013-03-12 14:56:06 +0000

    Command which checks that the issuer of the incoming SAML token matches the service provided value

---
+++ branches/GENESIS/9.0/Pep/srcBranch/java/it/intecs/pisa/toolbox/security/chain/commands/CheckSAMLTokenIssuer.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.intecs.pisa.toolbox.security.chain.commands;
+
+import javawebparts.misc.chain.ChainContext;
+import javawebparts.misc.chain.Command;
+import javawebparts.misc.chain.Result;
+
+import org.opensaml.SAMLAssertion;
+import org.opensaml.XML;
+
+import org.apache.axiom.soap.SOAPEnvelope;
+import org.apache.axiom.soap.SOAPHeader;
+
+import org.apache.axis2.context.MessageContext;
+import org.apache.axis2.util.XMLUtils;
+
+import org.apache.log4j.Logger;
+import org.w3c.dom.Element;
+
+/**
+ *
+ * @author mariarosaria
+ */
+public class CheckSAMLTokenIssuer implements Command {
+
+    static public String SAML_ASSERTION = "Assertion";
+    /*
+     * logger for the class
+     */
+    static Logger logger = Logger.getLogger(CheckSAMLTokenIssuer.class);
+    /*
+     * command property
+     */
+    private String issuer;
+
+    public void setIssuer(String inIssuer) {
+        issuer = inIssuer;
+    }
+
+    @Override
+    public Result init(ChainContext cc) {
+        return new Result(Result.SUCCESS);
+    }
+
+    @Override
+    public Result execute(ChainContext cc) {
+
+        try {
+            logger.info("Token issuer check started");
+            MessageContext msgCtx = (MessageContext) cc.getAttribute(CommandsConstants.MESSAGE_CONTEXT);
+            SOAPEnvelope envelope = msgCtx.getEnvelope();
+            SOAPHeader soapHeader = envelope.getHeader();
+
+            Element soapHeaderDOM = XMLUtils.toDOM(soapHeader);
+
+            Element assertion = (Element) soapHeaderDOM.getElementsByTagNameNS(XML.SAML_NS, SAML_ASSERTION).item(0);
+
+            SAMLAssertion saml = new SAMLAssertion(assertion);
+
+            String tokenIssuer = saml.getIssuer();
+            if (issuer.compareTo(tokenIssuer) == 0) {
+                logger.info("The token issuer is valid");
+                return new Result(Result.SUCCESS);
+            } else {
+                return new Result(Result.FAIL);
+            }
+
+        } catch (Exception e) {
+            logger.error("Error when checking token issuer: " + e.getMessage());
+            return new Result(Result.FAIL);
+        }
+    }
+
+    @Override
+    public Result cleanup(ChainContext cc) {
+        return new Result(Result.SUCCESS);
+    }
+}

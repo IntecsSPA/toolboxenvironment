c16d65a167a1b25923984115a34bb778622aa958
mariarosaria.7@gmail.com <mariarosaria.7@gmail.com@df02127e-7e62-ee8a-d59c-71b5b3ac54bb>
2013-05-29 13:18:08 +0000

    In the render function, the active tab is now set to the current active tab.

--- branches/GENESIS/9.0/Pep/web/import/xmlInterfaces/widgets/interface/extjs/ExtInterface.js
+++ branches/GENESIS/9.0/Pep/web/import/xmlInterfaces/widgets/interface/extjs/ExtInterface.js
@@ -355,6 +355,7 @@ function createPanelExjFormByXml(xmlDocument,lang, multiInterfaceId){
 
                 },
                 render: function(){
+                      var indexActiveTab = this.formsTab.getActiveTab();
                       var k,i,u,j;
                       if(this.formsArray.length > 1){
                         for(i=0; i<this.formsArray.length;i++){
@@ -365,7 +366,7 @@ function createPanelExjFormByXml(xmlDocument,lang, multiInterfaceId){
                               for(k=0; k<this.formsFileArray[i].length;k++)
                                 this.formsFileArray[i][k].render(document.getElementById(this.formsFileArray[i][k].divName));
                         }
-                        this.formsTab.setActiveTab(0);
+                        this.formsTab.setActiveTab(indexActiveTab);
                       }else{
                          if(this.formsArray[0].items.length >0)
                             this.formsArray[0].render(document.getElementById(this.formsArray[0].divName));
